a09450f385d8f46f69603d7e8e57de6fc24ece79
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2020-11-22 13:42:02 -0500

    reversestring completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// // reverse()
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// // for of loop
+// function reverse(str) {
+//   let reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// // forEach
+// function reverse(str) {
+//   let reversed = '';
+//   str.split('').forEach((char) => (reversed = char + reversed));
+
+//   return reversed;
+// }
+
+// reduce
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
