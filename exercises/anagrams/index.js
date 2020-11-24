7e368985190a2adf184e5201943d299b2f6b5463
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2020-11-24 12:02:06 -0500

    updated

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,44 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// use sort() to sort strings
+function anagrams(stringA, stringB) {
+  stringA = stringA
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+  stringB = stringB
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+
+  return stringA === stringB;
+}
+
+// // build charactor map
+// function anagrams(stringA, stringB) {
+//   stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+//   stringB = stringB.replace(/[^\w]/g, '').toLowerCase();
+//   if (stringA.length !== stringB.length) return false;
+
+//   const cache1 = {};
+//   stringA.split('').forEach((e) => (cache1[e] = (cache1[e] || 0) + 1));
+//   const cache2 = {};
+//   stringB.split('').forEach((e) => (cache2[e] = cache2[e] + 1 || 1));
+
+//   for (let key in cache1) {
+//     if (cache1[key] !== cache2[key]) return false;
+//   }
+
+//   return true;
+// }
+
+// console.log(anagrams('rail safety', 'fairy tales'));
+// console.log(anagrams('RAIL! SAFETY!', 'fairy tales'));
+// console.log(anagrams('Hi there', 'Bye there'));
 
 module.exports = anagrams;
