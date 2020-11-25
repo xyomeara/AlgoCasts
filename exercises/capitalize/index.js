cca0b44a4d080609aa51490ddaaf7a88a152eaa9
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2020-11-25 18:22:18 -0500

    updated

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,30 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//   const words = str.split(' ');
+//   // words.forEach((word, i) => words[i] = word[0].toUpperCase() + word.slice(1).toLowerCase());
+//   const capWords = words.map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase());
+//   return capWords.join(' ');
+// }
+
+// function capitalize(str) {
+//   const words = [];
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+//   }
+//   return words.join(' ');
+// }
+
+function capitalize(str) {
+  let newStr =str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    newStr += str[i - 1] === ' '? str[i].toUpperCase() : str[i]
+    
+  }
+  return newStr;
+}
+
+console.log(capitalize('a short sentence'));
 
 module.exports = capitalize;
