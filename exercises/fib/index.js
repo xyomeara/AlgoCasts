75cf19f2386e99c5386fe4c35f8073c870b51f37
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2020-11-30 14:07:40 -0500

    updated:

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   const fib = [0, 1];
+//   while (fib.length <= n){
+//     fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+//   }
+//   return fib[fib.length - 1];
+// }
+
+function fib(n, cache = {}) {
+  if (cache[n]) return cache[n];
+  if (n === 0) return (cache[0] = 0);
+  if (n === 1) return (cache[1] = 1);
+  return (cache[n] = fib(n - 1, cache) + fib(n - 2, cache));
+}
+
+// function fib(n) {
+//   if (n === 0) return 0;
+//   if (n === 1) return 1;
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+// function memorize(fn) {
+//   const cache = {};
+//   return function (...args) {
+//     if (cache[args]) return cache[args];
+//     let result = fn(...args);
+//     cache[args] = result;
+//     return result;
+//   };
+// }
+
+// fib = memorize(fib);
 
 module.exports = fib;
